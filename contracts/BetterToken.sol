3166a744260f975de3e70ee36195fec57589dff1
BlueCat <35709740+Ava520X@users.noreply.github.com>
2023-10-11 17:14:38 -0400

    Update BetterToken.sol

--- contracts/BetterToken.sol
+++ contracts/BetterToken.sol
@@ -7,7 +7,7 @@ import "../node_modules/zeppelin-solidity/contracts/token/ERC20/StandardToken.so
 contract BetterToken is StandardToken {
     string public name = "BetterToken";
     string public symbol = "BE";
-    uint8 public decimals = 10;
+    uint8 public decimals = 2;
     uint public INITIAL_SUPPLY = 12000;
 
     function BetterToken() public {
