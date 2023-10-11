558ee00142adaece198bb8733c9aebd592072f25
Coinbase <35709740+Ava520X@users.noreply.github.com>
2023-10-11 15:08:07 -0400

    Update and rename BeautterToken.sol to BetterToken.sol

--- contracts/BetterToken.sol
+++ contracts/BetterToken.sol
@@ -2,13 +2,13 @@ pragma solidity ^0.4.17;
 
 import "../node_modules/zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
-contract BeautterToken is StandardToken {
-    string public name = "BeautterToken";
+contract BetterToken is StandardToken {
+    string public name = "BetterToken";
     string public symbol = "BE";
     uint8 public decimals = 2;
     uint public INITIAL_SUPPLY = 12000;
 
-    function BeautterToken() public {
+    function BetterToken() public {
         totalSupply_ = INITIAL_SUPPLY;
         balances[msg.sender] = INITIAL_SUPPLY;
     }
