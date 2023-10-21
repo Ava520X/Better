bcc869e2130e69843ad1ac149354058a9d4fe340
BlueCat <35709740+Soullog246@users.noreply.github.com>
2023-10-21 16:42:58 -0400

    Update and rename BetterToken.sol to SpaToken.sol
    Fruit Body Spa

--- contracts/SpaToken.sol
+++ contracts/SpaToken.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.4.17;
 import "../node_modules/zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
 contract BetterToken is StandardToken {
-    string public name = "BetterToken";
+    string public name = "BetteToken";
     string public symbol = "BE";
     uint8 public decimals = 2;
     uint public INITIAL_SUPPLY = 12000;
